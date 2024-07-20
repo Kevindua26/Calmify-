3df127fca4d3f854e880684f93634bf1f40d605d
Raunak Madan <madanraunak24@gmail.com>
2024-07-20 18:05:34 +0530

    Update route for reels and character

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Character from "../pages/Character";
 import Home from "../pages/Home";
+import Reels from "../pages/Reels";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/character",
         element: <Character />,
+      },
+      {
+        path: "/reels",
+        element: <Reels />,
       },
     ],
   },
