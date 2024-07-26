f2238a525aa57dcd129145186540ec28324833a0
Raunak Madan <madanraunak24@gmail.com>
2024-07-26 21:06:54 +0530

    Update routes for mood and games

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,6 +4,12 @@ import Character from "../pages/Character";
 import Home from "../pages/Home";
 import Reels from "../pages/Reels";
 import ReelDetail from "../pages/ReelDetail";
+import Games from "../pages/Games";
+import SimonSays from "../pages/SimonSays";
+import FlappyBirdGame from "../pages/FlappyBirdGame";
+import ColorMatch from "../pages/ColorMatch";
+import Mood from "../pages/Mood";
+import Input from "../components/Mood/Input";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +31,26 @@ const router = createBrowserRouter([
       {
         path: "/reels/:id",
         element: <ReelDetail />,
+      },
+      {
+        path: "/games",
+        element: <Games />,
+      },
+      {
+        path: "/simon",
+        element: <SimonSays />,
+      },
+      {
+        path: "/flappy",
+        element: <FlappyBirdGame />,
+      },
+      {
+        path: "/colormatch",
+        element: <ColorMatch />,
+      },
+      {
+        path:"/mood",
+        element: <Mood/>,
       },
     ],
   },
